9004fea6b13c22bd879ac2d415df8c5b2885527e
Massimo Zaniboni <mzan@dokmelody.org>
2018-11-07 15:39:56 +0100

    More modular importing of CDRS
    Ref: #30

--- rating_tools/rate_engine/src/Asterisell/ImportDataFiles.hs
+++ rating_tools/rate_engine/src/Asterisell/ImportDataFiles.hs
@@ -2,7 +2,9 @@
 
 -- SPDX-License-Identifier: GPL-3.0-or-later
 
--- | Import CDS from external files or external databases inside `ar_source_cdr` table.
+-- | Import CDRS from external files or external databases inside `ar_source_cdr` table.
+--   Signal only problems about callDate, because all other problems will be reported
+--   during rating.
 --
 module Asterisell.ImportDataFiles (
   SourceDataSummaryInfo,
@@ -39,16 +41,6 @@ import Text.Heredoc
 -- ---------------------------------
 -- Params
 
--- | Number of CDRs to process in each phase: parse, rate, write to DB.
---   A bigger chunk reduces the number of DB round-trips, because more CDRs are sent to the DB,
---   and maximize the CPU cache usage.
---   A bigger chunk optimizes also the CPU cache usage, because the same work is done on a chunk of CDRS,
---   instead of changing for each CDR the working context. So in each phase the results are forced to be
---   under DeepSeq, and fully evaluated.
-param_chunkSize :: Int
-param_chunkSize = 512
-{-# INLINE param_chunkSize #-}
-
 -- | The number of source CDRs to fetch from the DB, before splitting the rating event.
 --   Up to date the Haskell MySQL driver does not support the forward-only cursor mode,
 --   and it stores all the CDRS of the rating query in RAM.
@@ -70,76 +62,10 @@ type SourceDataSummaryInfo
     )
 
 -- -------------------------------------------------------------------
--- Initial Import of source/raw CDRS
-
--- | Convert the content of an input file into a stream of CDRS.
---   Return errors only if the call date can not be parsed, because this phase
---   favours importing of data at all costs,
---   while other errors are returned in other processing phases.
-streamOfRawCDRS
-  :: forall a . CDRFormat a
-  => AType a
-  -> SourceCDRParams
-  -- ^ the stream has CDRs in the same physical format
-  -> CDRProviderName
-  -> InputStream RawSourceCDR
-  -- ^ the CDRs in source/raw format
-  -> IO (InputStream (RawSourceCDR
-                     -- ^ the CDR in raw format. Mainly for displaying debug information, found in later phase of rating
-                     , Either AsterisellError LocalTime
-                     -- ^ an error if the CDR date can not be parsed correctly,
-                     -- otherwise the calldate and the (if present) the external telephone numbers.
-                     ))
-
-streamOfRawCDRS cdrType params providerName inputStream
-    = allLines inputStream >>= maybeRemoveHeader >>= utf8Lines >>= (S.map process) >>= stream_toJust
- where
-
-   process :: RawSourceCDR -> Maybe (RawSourceCDR, Either AsterisellError LocalTime)
-   process rawCDR
-     = case (parseTypedRawSourceCDRToCallDate cdrType params 4 providerName rawCDR) of
-         Left err
-           -> Just $ (rawCDR, Left err)
-         Right Nothing
-           -> Nothing
-         Right (Just callDate)
-           -> Just $ (rawCDR, Right callDate)
-
-   utf8Lines stream1
-     = case params of
-         SourceCDRParamsCSVFile _ _ _ UseUTF8
-           -> return stream1
-
-         -- _ -> error "Parsing of CSV files not in UTF8 format is not yet supported by the application. Contact the assistance. "
-         -- NOTE:  up to date not needed because `UseUTF8`
-
-   maybeRemoveHeader stream1
-     = case params of
-         SourceCDRParamsCSVFile (Just _) _ _ _
-           -> S.drop 1 stream1
-         _ -> return stream1
-
-   allLines :: InputStream BS.ByteString -> IO (InputStream RawSourceCDR)
-   allLines stream1
-     = case params of
-         SourceCDRParamsCSVFile _ _ False _
-           -> do stream2 <- S.splitOn (\c -> c == '\n' || c == '\r') stream1
-                 stream3 <- S.filter (\s -> not $ BS.null s) stream2
-                 return stream3
-         _ -> error "Parsing of CSV files with new lines inside quoted fields is not yet supported by the application. Contact the assistance. "
-
+-- File as source of CDRS
 
 -- | Parse a file and import into database as ar_source_cdr records, returning summary info on its content.
---   Report only serious errors, because all errors specific of CDRs will be reported during rating phase.
---   CDRs without a clear calldate, will be imported using the previous calldate, or the calldate of today.
 --
--- The design is this:
--- * in a first phase source data files, with source CDRS are imported into DB, ordered by calldate
--- * only the calldate is parsed
--- * the source CDRs can be ordered by call date directly from the DB
--- * during rating phases the source CDRs are read by calldate from the DB, and the string with the content is converted to a CDR and rated
--- * the engine signal if after the changing of a parsing algo, the calldate is interpreted in a different way
--- * this code allows for changes of the code interpreting CDRS, and safe order by calldate, needed by bundle rates
 rateEngine_importDataFile
   :: FilePath
   -> CDRProviderName
@@ -167,6 +93,111 @@ rateEngine_importDataFile
   dbConf
   precision = do
 
+    (CDRFormatSpec !params !theType)
+      <- case getSupportedCDRSImporters logicalTypeName formatTypeName of
+           Nothing
+             -> throwIO $ AsterisellException ("There is no known/configured source data file importer for format " ++ (Text.unpack logicalTypeName) ++ ", and version " ++  (Text.unpack formatTypeName))
+           Just r
+             -> return r
+
+    withResource'
+      (return ())
+      (\_ -> S.withFileAsInput
+               inputFile
+               (\fileContentStream -> do
+                   rawCDRS <- (allLines params fileContentStream) >>= (maybeRemoveHeader params) >>= (utf8Lines params)
+
+                   rateEngine_importIntoDB
+                     providerName
+                     providerId
+                     logicalTypeName
+                     logicalTypeId
+                     formatTypeName
+                     formatTypeId
+                     isStatusFile
+                     maybeStatusTimeFrame
+                     dbConf
+                     precision
+                     rawCDRS
+                     (\ conn -> void $ DB.execute conn  "REPLACE INTO ar_local_file_to_delete(name) VALUES(?)" [toDBText $ Text.pack inputFile])
+               ))
+
+      (\_ _ -> return ())
+      (createImportError)
+
+  where
+
+   utf8Lines params stream1
+       = case params of
+         SourceCDRParamsCSVFile _ _ _ UseUTF8
+           -> return stream1
+
+         -- _ -> error "Parsing of CSV files not in UTF8 format is not yet supported by the application. Contact the assistance. "
+         -- NOTE:  up to date not needed because `UseUTF8`
+
+   maybeRemoveHeader params stream1
+     = case params of
+         SourceCDRParamsCSVFile (Just _) _ _ _
+           -> S.drop 1 stream1
+         _ -> return stream1
+
+   allLines :: SourceCDRParams -> InputStream BS.ByteString -> IO (InputStream RawSourceCDR)
+   allLines params stream1
+     = case params of
+         SourceCDRParamsCSVFile _ _ False _
+           -> do stream2 <- S.splitOn (\c -> c == '\n' || c == '\r') stream1
+                 stream3 <- S.filter (\s -> not $ BS.null s) stream2
+                 return stream3
+         _ -> error "Parsing of CSV files with new lines inside quoted fields is not yet supported by the application. Contact the assistance. "
+
+   createImportError e
+     = SomeException $ AsterisellException $ "There is an error during the importing of file \"" ++ inputFile ++ "\". " ++ (displayException e)
+
+-- --------------------------------------------
+-- MySQL database as external source of CDRS
+
+-- TODO
+
+-- ----------------------------
+-- Write data to the DB
+
+-- | Insert a stream of source CDRS inside `ar_source_cdr`.
+--
+--   Report only serious errors, because all errors specific of CDRs will be reported during rating phase.
+--
+--   CDRs without a clear calldate, will be imported using the previous calldate, or the calldate of today.
+--
+rateEngine_importIntoDB
+  :: CDRProviderName
+  -> Int
+  -> Text.Text
+  -> Int
+  -> Text.Text
+  -> Int
+  -> Bool
+  -> Maybe (LocalTime, LocalTime)
+  -> DBConf
+  -> CurrencyPrecisionDigits
+  -> InputStream RawSourceCDR
+  -- ^ the CDRs in source/raw format
+  -> (DB.MySQLConn -> IO ())
+  -- ^ an action for annotating successfully imports
+  -> IO SourceDataSummaryInfo
+
+rateEngine_importIntoDB
+  providerName
+  providerId
+  logicalTypeName
+  logicalTypeId
+  formatTypeName
+  formatTypeId
+  isStatusFile
+  maybeStatusTimeFrame
+  dbConf
+  precision
+  inStream1
+  commitAction = do
+
    nrOfRatingJobs <- process_initCores 0
 
    (CDRFormatSpec !sourceCDRParams !theType)
@@ -183,13 +214,13 @@ rateEngine_importDataFile
          db_openTransaction conn
          return conn)
      (\conn -> do
- 
+
        when isStatusFile (db_deleteFromArSource conn formatTypeId providerId maybeStatusTimeFrame)
 
        nowLocalTime1 <- getZonedTime
        let callDate0 = zonedTimeToLocalTime nowLocalTime1
        let summaryInfo0 = (Nothing, 0, 0)
-       -- NOTE: use an MVar instead of an IORef, because I had space-leak problems
+       -- NOTE: use an MVar instead of an IORef, because I had space-leak problems 
 
        (rawCDRSChan, writeToDBProcess)
          <- db_loadDataFromNamedPipe
@@ -198,14 +229,20 @@ rateEngine_importDataFile
               "ar_source_cdr"
               sourceCDR_mysqlCSVLoadCmd
               sourceCDR_toMySQLCSV
+
+       (_, summaryInfo@(maybeTimeFrame, totLines, linesWithErrors)) <- do
+         let parseCallDate rawCDR
+               = case (parseTypedRawSourceCDRToCallDate theType sourceCDRParams 4 providerName rawCDR) of
+                   Left err -> Just $ (rawCDR, Left err)
+                   Right Nothing -> Nothing
+                   Right (Just callDate) -> Just $ (rawCDR, Right callDate)
  
-       (_, summaryInfo@(maybeTimeFrame, totLines, linesWithErrors))
-         <- S.withFileAsInput inputFile $ \fileContentStream -> do
-              inStream <- (streamOfRawCDRS theType sourceCDRParams providerName fileContentStream)
-              !r <- S.foldM (\ (!s) (!cdr) -> sendToDB rawCDRSChan s cdr) (callDate0, summaryInfo0) inStream
-              putMVar rawCDRSChan Nothing
-              waitAll (V.toList writeToDBProcess) []
-              return $ DeepSeq.force r
+         inStream2 <- (S.map parseCallDate inStream1) >>= stream_toJust
+         !r <- S.foldM (\ (!s) (!cdr) -> sendToDB rawCDRSChan s cdr) (callDate0, summaryInfo0) inStream2
+
+         putMVar rawCDRSChan Nothing
+         _ <- waitAll (V.toList writeToDBProcess) []
+         return $ DeepSeq.force r
 
        -- Update the rating frame again.
        case totLines > 0 && linesWithErrors == totLines of
@@ -218,10 +255,10 @@ rateEngine_importDataFile
                        Nothing -> do return ()
                        Just (rateFromCallDate, _) -> db_updateRatingTimeFrame conn rateFromCallDate
 
+       commitAction conn
+
        return summaryInfo)
      (\isOk conn -> do
-         when isOk (do _ <- DB.execute conn  "REPLACE INTO ar_local_file_to_delete(name) VALUES(?)" [toDBText $ Text.pack inputFile] ; return ())
-         -- NOTE: if the transaction abort, the file (correctly) will not be signaled as to delete
          db_releaseResource isOk conn)
      (createImportError)
 
@@ -252,12 +289,15 @@ rateEngine_importDataFile
              return $ DeepSeq.force (lastCallDate', summaryInfo')
 
    createImportError e
-     = SomeException $ AsterisellException $ "There is an error during the importing of file \"" ++ inputFile ++ "\", with provider " ++ (Text.unpack providerName) ++ ", format " ++  (Text.unpack logicalTypeName) ++ "__" ++  (Text.unpack formatTypeName) ++ ". " ++ (displayException e) ++ ".\nAll the source data files with this format, and/or similar problems, will be not imported and rated. So there can be an high number of CDRs that are not rated. Note that there is only one error message for a file of this type, but this error message implies that all files of the same type are not rated.\nThis is probably an error in the application configuration. Contact the assistance."
+     = SomeException $ AsterisellException $ "There is a critical error during the importing from provider " ++ (Text.unpack providerName) ++ ", format " ++  (Text.unpack logicalTypeName) ++ "__" ++  (Text.unpack formatTypeName) ++ ". " ++ (displayException e) ++ ".\nAll data from the provider or with the same format will be not rated.\nThis is probably an error in the application configuration. Contact the assistance."
 
    {-# INLINE newMaxMinCallDate #-}
    newMaxMinCallDate :: LocalTime -> Maybe (LocalTime, LocalTime) -> Maybe (LocalTime, LocalTime)
    newMaxMinCallDate d Nothing = Just (d, d)
    newMaxMinCallDate d (Just (minDD, maxDD)) = Just (min minDD d, max maxDD d)
+
+-- ---------------------------------------
+-- Export CDRS
 
 -- | Export the CDRs in a human processable format.
 --   Usually the export is done for fixing errors in the original source CDRs.
@@ -404,5 +444,3 @@ db_updateRatingTimeFrame conn fromDate = do
 
   DB.execute conn query [toDBLocalTime fromDate, toDBLocalTime fromDate]
   return ()
-
-
